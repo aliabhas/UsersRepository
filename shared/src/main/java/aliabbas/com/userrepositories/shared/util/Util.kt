f5079d8cd93fd7cbf9c2c52dc9c2b321b1c565a5
aliabhas <aliabhaas@gmail.com>
2022-04-30 02:07:17 +0500

    Few Comments and Package changes

--- shared/src/main/java/aliabbas/com/userrepositories/shared/util/Util.kt
+++ shared/src/main/java/aliabbas/com/userrepositories/shared/util/Util.kt
@@ -13,36 +13,11 @@ import android.net.NetworkInfo
 class Util {
 
     companion object {
-
         public fun isNetworkAvailable(context: Context): Boolean {
             val connectivityManager =
                 context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
             val activeNetworkInfo: NetworkInfo? = connectivityManager!!.activeNetworkInfo
             return activeNetworkInfo != null && activeNetworkInfo.isConnected
         }
-
-        /* fun isNetworkAvailableRealTime(context: Context) {
-             val connectivityManager =
-                 context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                 connectivityManager.registerDefaultNetworkCallback(networkCallback)
-             } else {
-                 val request: NetworkRequest = NetworkRequest.Builder()
-                     .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET).build()
-                 connectivityManager.registerNetworkCallback(request, networkCallback)
-             }
-         }
-
-         private var networkCallback: ConnectivityManager.NetworkCallback =
-             object : ConnectivityManager.NetworkCallback() {
-                 override fun onAvailable(network: Network) {
-                     Log.i("Network Call", "Network Call true")
-                 }
-
-                 override fun onLost(network: Network) {
-                     Log.i("Network Call", "Network Call false")
-                 }
-             }*/
     }
 }
